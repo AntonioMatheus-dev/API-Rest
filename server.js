5f1dea8452afc3a42ee039bb0929c18d6b90ede5
AntonioMatheus-dev <matheus69969@gmail.com>
2025-12-31 16:36:47 -0300

    adiciona o arquivo server.js para iniciar o servidor na porta 3000

---
+++ server.js
@@ -0,0 +1,12 @@
+import app from "./src/app.js";
+
+
+const PORTA = 3000
+
+//Fazer conecxão
+
+app.listen(PORTA, ()=>{
+    console.log(`Executando o Servidor no endereço http://localhost:${PORTA}`)
+});
+
+
