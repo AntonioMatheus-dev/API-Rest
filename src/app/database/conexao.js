12f3052387ec60c12bbd5e5fa64221338c0015b8
AntonioMatheus-dev <matheus69969@gmail.com>
2025-12-31 16:55:35 -0300

    atualiza configuração da conexão com o banco de dados para usar variáveis de ambiente

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -1,15 +1,16 @@
-// infra/conexao.js
+import "dotenv/config";
+console.log(process.env.DB_HOST, process.env.DB_PORT);
 import pkg from "pg";
 const { Client } = pkg;
 
 const conexao = new Client({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "mat.1127",
-  database: "bdcopa",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-conexao.connect()
+conexao.connect();
 
 export default conexao;
